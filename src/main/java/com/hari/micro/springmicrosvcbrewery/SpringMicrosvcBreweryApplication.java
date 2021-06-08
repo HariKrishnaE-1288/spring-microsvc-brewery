3e4e836d29165c25aa02b755dd24391eae63491b
HariKrishnaE-1288 <56486503+HariKrishnaE-1288@users.noreply.github.com>
2021-06-08 12:31:03 +0530

    Commit with correct artifact id

---
+++ src/main/java/com/hari/micro/springmicrosvcbrewery/SpringMicrosvcBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.hari.micro.springmicrosvcbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicrosvcBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicrosvcBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hari/micro/springmicrosvcbrewery/SpringMicrosvcBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hari.micro.springmicrosvcbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicrosvcBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
